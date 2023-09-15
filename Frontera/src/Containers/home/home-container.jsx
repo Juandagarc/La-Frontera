77e5b0d095f4d5899724ca75179579d13991191a
Juandagarc <juandagarce@gmail.com>
2023-09-14 20:25:07 -0500

    maps

--- Frontera/src/Containers/home/home-container.jsx
+++ Frontera/src/Containers/home/home-container.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/frontera-logo.png'
+import Ubicanos from '../../assets/ubicacion.png'
 import Footer from '../../components/footer/footer';
 import "./home-container.css"
 import { Link } from "react-router-dom";
@@ -11,7 +12,11 @@ const HomeContainer = () => {
 
             <div className='list-container'>
                 <p className='list-container text'> Echa un vistazo a nuestro delicioso menú aquí en La Frontera</p>
-                    <button className='list-container button' > <Link to="/Carta">ver menú</Link></button>
+                    <button className='list-container button' > <Link to="/Carta">Ver menú.</Link></button>
+            </div>
+            <a href='https://goo.gl/maps/UERcBFUMHjNZFQRu9'><img src={Ubicanos} alt="maps" className='img-maps' /></a>
+            <div className='list-container'>
+            <button className='list-container button' > <a href='https://goo.gl/maps/UERcBFUMHjNZFQRu9'>Ubicación.</a></button>
             </div>
             <Footer />
         </div>
